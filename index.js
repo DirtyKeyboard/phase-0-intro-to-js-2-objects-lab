20220c1a022c94fc868bbc3e188c1938771a88d7
Andrew Hawileh <andrewh829@gmail.com>
2022-10-04 02:09:53 -0500

    commit tree 861a2bfc39a0b3fe4c7673a2f6ca3f982df2cdc7

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name : "Andrew Hawileh",
+    streetAddress : "1550 W Plano Pkwy"
+}
+
+function updateEmployeeWithKeyAndValue(empObj, key, value)
+{
+    return {...empObj, [key]: value}
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(empObj, key, value)
+{
+    empObj[key] = value;
+    return empObj;
+}
+
+function deleteFromEmployeeByKey(empObj, key)
+{
+    const obRet = {...empObj};
+    delete obRet[key];
+    return obRet;
+}
+
+function destructivelyDeleteFromEmployeeByKey(empObj, key)
+{
+    delete empObj[key];
+    return empObj;
+}
