fd121562b0dc6a7fe165c042fa6888ba190f7645
Andrew Hawileh <andrewh829@gmail.com>
2023-02-24 08:18:55 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = 
+{
+    name: "Andrew H",
+    streetAddress: "1550 W Plano Pkwy"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value)
+{
+    const temp = {...employee};
+    temp[key] = value;
+    return temp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+{
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key)
+{
+    const temp = {...employee}
+    delete temp[key];
+    return temp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key)
+{
+    delete employee[key];
+    return employee;
+}
